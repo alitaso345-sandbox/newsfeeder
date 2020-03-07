e93ebabc2d5b5c2bbe3efc5743bf49ca182c5548
alitaso345 <alice.maru345@gmail.com>
2020-03-07 18:47:53 +0900

    Add endpoints

--- httpd/main.go
+++ httpd/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"github.com/alitaso345-sandbox/newsfeeder/httpd/handler"
+	"github.com/alitaso345-sandbox/newsfeeder/platform/newsfeed"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	feed := newsfeed.New()
 	r := gin.Default()
 
 	r.GET("/ping", handler.PingGet())
+	r.GET("/newsfeed", handler.NewsfeedGet(feed))
+	r.POST("/newsfeed", handler.NewsfeedPost(feed))
 
 	r.Run()
 }
